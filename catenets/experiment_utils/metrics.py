d62e74a0519102918573c3aa932503efa9db07ce
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-08-02 12:40:22 +0100

    update metrics params

--- catenets/experiment_utils/metrics.py
+++ catenets/experiment_utils/metrics.py
@@ -2,27 +2,27 @@
 import torch
 
 
-def sqrt_PEHE(y: torch.Tensor, hat_y: torch.Tensor) -> torch.Tensor:
+def sqrt_PEHE(po: torch.Tensor, hat_te: torch.Tensor) -> torch.Tensor:
     """
     Precision in Estimation of Heterogeneous Effect(PyTorch version).
     PEHE reflects the ability to capture individual variation in treatment effects.
     Args:
-        y: expected outcome.
-        hat_y: estimated outcome.
+        po: expected outcome.
+        hat_te: estimated outcome.
     """
-    y = torch.Tensor(y)
-    hat_y = torch.Tensor(hat_y)
-    return torch.sqrt(torch.mean(((y[:, 1] - y[:, 0]) - hat_y) ** 2))
+    po = torch.Tensor(po)
+    hat_te = torch.Tensor(hat_te)
+    return torch.sqrt(torch.mean(((po[:, 1] - po[:, 0]) - hat_te) ** 2))
 
 
-def abs_error_ATE(y: torch.Tensor, hat_y: torch.Tensor) -> torch.Tensor:
+def abs_error_ATE(po: torch.Tensor, hat_te: torch.Tensor) -> torch.Tensor:
     """
     Average Treatment Effect.
     ATE measures what is the expected causal effect of the treatment across all individuals in the population.
     Args:
-        y: expected outcome.
-        hat_y: estimated outcome.
+        po: expected outcome.
+        hat_te: estimated outcome.
     """
-    y = torch.Tensor(y)
-    hat_y = torch.Tensor(hat_y)
-    return torch.abs(torch.mean(y[:, 1] - y[:, 0]) - torch.mean(hat_y))
+    po = torch.Tensor(po)
+    hat_te = torch.Tensor(hat_te)
+    return torch.abs(torch.mean(po[:, 1] - po[:, 0]) - torch.mean(hat_te))
