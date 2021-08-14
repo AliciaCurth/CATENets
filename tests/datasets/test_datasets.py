0e3cd95fb22a2a13664fa70d7afa41cd020f9e9d
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-08-14 14:28:56 +0100

    ACIC2016 dataset

--- tests/datasets/test_datasets.py
+++ tests/datasets/test_datasets.py
@@ -34,3 +34,14 @@ def test_dataset_sanity_ihdp() -> None:
     assert X_train.shape[0] == Y_train_full.shape[0]
     assert X_train.shape[0] == W_train.shape[0]
     assert X_test.shape[0] == Y_test.shape[0]
+
+
+@pytest.mark.slow
+def test_dataset_sanity_acic2016() -> None:
+    X_train, W_train, Y_train, Y_train_full, X_test, Y_test = load("acic2016")
+
+    assert X_train.shape[1] == X_test.shape[1]
+    assert X_train.shape[0] == Y_train.shape[0]
+    assert X_train.shape[0] == Y_train_full.shape[0]
+    assert X_train.shape[0] == W_train.shape[0]
+    assert X_test.shape[0] == Y_test.shape[0]
