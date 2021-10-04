ef65a7de2a72ab3b4d7906ed9b2ddb73880932a6
AliciaCurth <37110263+AliciaCurth@users.noreply.github.com>
2021-10-04 11:39:13 +0200

    Add Torch SNet without propensity estimators (#7)
    * Add Torch SNet without propensity estimators
    
    * Update snet
    
    * update snet tests
    
    Co-authored-by: AliciaCurth <aliciacurth@gmail.com>

--- tests/models/torch/test_torch_snet.py
+++ tests/models/torch/test_torch_snet.py
@@ -8,6 +8,7 @@ from catenets.models.torch import SNet
 
 
 def test_model_params() -> None:
+    # with propensity estimator
     model = SNet(
         2,
         binary_y=True,
@@ -41,6 +42,42 @@ def test_model_params() -> None:
     assert len(model._reps_mu1.model) == 9
     assert len(model._propensity_estimator.model) == 8
 
+    # remove propensity estimator
+    model = SNet(
+        2,
+        binary_y=True,
+        n_layers_out=1,
+        n_units_out=2,
+        n_layers_r=3,
+        n_units_r=4,
+        weight_decay=0.5,
+        lr=0.6,
+        n_iter=700,
+        batch_size=80,
+        val_split_prop=0.9,
+        n_iter_print=10,
+        seed=11,
+        with_prop=False
+    )
+
+    with np.testing.assert_raises(AttributeError):
+        model._reps_c
+    with np.testing.assert_raises(AttributeError):
+        model._reps_prop
+    with np.testing.assert_raises(AttributeError):
+        model._propensity_estimator
+    assert model._reps_o is not None
+    assert model._reps_mu0 is not None
+    assert model._reps_mu1 is not None
+    assert len(model._po_estimators) == 2
+
+    for mod in model._po_estimators:
+        assert len(mod.model) == 5  # 1 in + NL + 4 * (n_layers_out - 1) + 1 out + NL
+
+    assert len(model._reps_o.model) == 9
+    assert len(model._reps_mu0.model) == 9
+    assert len(model._reps_mu1.model) == 9
+
 
 @pytest.mark.parametrize("nonlin", ["elu", "relu", "sigmoid", "selu", "leaky_relu"])
 def test_model_params_nonlin(nonlin: str) -> None:
@@ -73,6 +110,7 @@ def test_model_sanity(dataset: str, pehe_threshold: float) -> None:
     X_train, W_train, Y_train, Y_train_full, X_test, Y_test = load(dataset)
     W_train = W_train.ravel()
 
+    # with propensity estimator
     model = SNet(
         X_train.shape[1],
         binary_y=(len(np.unique(Y_train)) == 2),
@@ -85,6 +123,21 @@ def test_model_sanity(dataset: str, pehe_threshold: float) -> None:
     )
 
     print(f"Evaluation for model SNet on {dataset} = {score['str']}")
+    assert score["raw"]["pehe"][0] < pehe_threshold
+
+    model = SNet(
+        X_train.shape[1],
+        binary_y=(len(np.unique(Y_train)) == 2),
+        batch_size=1024,
+        n_iter=1500,
+        with_prop=False
+    )
+
+    score = evaluate_treatments_model(
+        model, X_train, Y_train, Y_train_full, W_train, n_folds=3
+    )
+
+    print(f"Evaluation for model SNet (with_prop=False) on {dataset} = {score['str']}")
     assert score["raw"]["pehe"][0] < pehe_threshold
 
 
