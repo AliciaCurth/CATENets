b545d6045bb28787b8eb9cd3e960b5bbdf996e10
AliciaCurth <aliciacurth@gmail.com>
2021-10-04 10:15:36 +0100

    Add Torch SNet without propensity estimators

--- catenets/models/torch/snet.py
+++ catenets/models/torch/snet.py
@@ -90,30 +90,31 @@ class SNet(BaseCATEEstimator):
     """
 
     def __init__(
-        self,
-        n_unit_in: int,
-        binary_y: bool = False,
-        n_layers_r: int = DEFAULT_LAYERS_R,
-        n_units_r: int = DEFAULT_UNITS_R_BIG_S,
-        n_layers_out: int = DEFAULT_LAYERS_OUT,
-        n_units_r_small: int = DEFAULT_UNITS_R_SMALL_S,
-        n_units_out: int = DEFAULT_UNITS_OUT,
-        n_units_out_prop: int = DEFAULT_UNITS_OUT,
-        n_layers_out_prop: int = DEFAULT_LAYERS_OUT,
-        weight_decay: float = DEFAULT_PENALTY_L2,
-        penalty_orthogonal: float = DEFAULT_PENALTY_ORTHOGONAL,
-        penalty_disc: float = DEFAULT_PENALTY_DISC,
-        lr: float = DEFAULT_STEP_SIZE,
-        n_iter: int = DEFAULT_N_ITER,
-        batch_size: int = DEFAULT_BATCH_SIZE,
-        val_split_prop: float = DEFAULT_VAL_SPLIT,
-        n_iter_print: int = DEFAULT_N_ITER_PRINT,
-        seed: int = DEFAULT_SEED,
-        nonlin: str = DEFAULT_NONLIN,
-        ortho_reg_type: str = "abs",
-        patience: int = DEFAULT_PATIENCE,
-        clipping_value: int = 1,
-        batch_norm: bool = True
+            self,
+            n_unit_in: int,
+            binary_y: bool = False,
+            n_layers_r: int = DEFAULT_LAYERS_R,
+            n_units_r: int = DEFAULT_UNITS_R_BIG_S,
+            n_layers_out: int = DEFAULT_LAYERS_OUT,
+            n_units_r_small: int = DEFAULT_UNITS_R_SMALL_S,
+            n_units_out: int = DEFAULT_UNITS_OUT,
+            n_units_out_prop: int = DEFAULT_UNITS_OUT,
+            n_layers_out_prop: int = DEFAULT_LAYERS_OUT,
+            weight_decay: float = DEFAULT_PENALTY_L2,
+            penalty_orthogonal: float = DEFAULT_PENALTY_ORTHOGONAL,
+            penalty_disc: float = DEFAULT_PENALTY_DISC,
+            lr: float = DEFAULT_STEP_SIZE,
+            n_iter: int = DEFAULT_N_ITER,
+            batch_size: int = DEFAULT_BATCH_SIZE,
+            val_split_prop: float = DEFAULT_VAL_SPLIT,
+            n_iter_print: int = DEFAULT_N_ITER_PRINT,
+            seed: int = DEFAULT_SEED,
+            nonlin: str = DEFAULT_NONLIN,
+            ortho_reg_type: str = "abs",
+            patience: int = DEFAULT_PATIENCE,
+            clipping_value: int = 1,
+            batch_norm: bool = True,
+            with_prop: bool = True
     ) -> None:
         super(SNet, self).__init__()
 
@@ -129,14 +130,8 @@ class SNet(BaseCATEEstimator):
         self.ortho_reg_type = ortho_reg_type
         self.clipping_value = clipping_value
         self.patience = patience
+        self.with_prop = with_prop
 
-        self._reps_c = RepresentationNet(
-            n_unit_in, n_units=n_units_r, n_layers=n_layers_r, nonlin=nonlin, batch_norm=batch_norm
-        )
-        self._reps_o = RepresentationNet(
-            n_unit_in, n_units=n_units_r_small, n_layers=n_layers_r, nonlin=nonlin,
-            batch_norm=batch_norm
-        )
         self._reps_mu0 = RepresentationNet(
             n_unit_in, n_units=n_units_r_small, n_layers=n_layers_r, nonlin=nonlin,
             batch_norm=batch_norm
@@ -145,57 +140,97 @@ class SNet(BaseCATEEstimator):
             n_unit_in, n_units=n_units_r_small, n_layers=n_layers_r, nonlin=nonlin,
             batch_norm=batch_norm
         )
-        self._reps_prop = RepresentationNet(
-            n_unit_in, n_units=n_units_r, n_layers=n_layers_r, nonlin=nonlin,
-            batch_norm=batch_norm
-        )
 
         self._po_estimators = []
-        for idx in range(2):
-            self._po_estimators.append(
-                BasicNet(
-                    f"snet_po_estimator_{idx}",
-                    n_units_r
-                    + n_units_r_small
-                    + n_units_r_small,  # (reps_c, reps_o, reps_mu{idx})
-                    binary_y=binary_y,
-                    n_layers_out=n_layers_out,
-                    n_units_out=n_units_out,
-                    nonlin=nonlin,
-                    batch_norm=batch_norm
-                )
-            )
-        self._propensity_estimator = PropensityNet(
-            "snet_propensity_estimator",
-            n_units_r + n_units_r,  # eps_c, reps_w
-            2,
-            "prop",
-            n_layers_out_prop=n_layers_out_prop,
-            n_units_out_prop=n_units_out_prop,
-            nonlin=nonlin,
-            batch_norm=batch_norm
-        ).to(DEVICE)
 
-        params = (
-            list(self._reps_c.parameters())
-            + list(self._reps_o.parameters())
-            + list(self._reps_mu0.parameters())
-            + list(self._reps_mu1.parameters())
-            + list(self._reps_prop.parameters())
-            + list(self._po_estimators[0].parameters())
-            + list(self._po_estimators[1].parameters())
-            + list(self._propensity_estimator.parameters())
-        )
+        if self.with_prop:
+            self._reps_c = RepresentationNet(
+                n_unit_in, n_units=n_units_r, n_layers=n_layers_r, nonlin=nonlin,
+                batch_norm=batch_norm
+            )
+
+            self._reps_o = RepresentationNet(
+                n_unit_in, n_units=n_units_r_small, n_layers=n_layers_r, nonlin=nonlin,
+                batch_norm=batch_norm
+            )
+
+            self._reps_prop = RepresentationNet(
+                n_unit_in, n_units=n_units_r, n_layers=n_layers_r, nonlin=nonlin,
+                batch_norm=batch_norm
+            )
+
+            for idx in range(2):
+                self._po_estimators.append(
+                    BasicNet(
+                        f"snet_po_estimator_{idx}",
+                        n_units_r
+                        + n_units_r_small
+                        + n_units_r_small,  # (reps_c, reps_o, reps_mu{idx})
+                        binary_y=binary_y,
+                        n_layers_out=n_layers_out,
+                        n_units_out=n_units_out,
+                        nonlin=nonlin,
+                        batch_norm=batch_norm
+                    )
+                )
+            self._propensity_estimator = PropensityNet(
+                "snet_propensity_estimator",
+                n_units_r + n_units_r,  # reps_c, reps_w
+                2,
+                "prop",
+                n_layers_out_prop=n_layers_out_prop,
+                n_units_out_prop=n_units_out_prop,
+                nonlin=nonlin,
+                batch_norm=batch_norm
+            ).to(DEVICE)
+
+            params = (
+                    list(self._reps_c.parameters())
+                    + list(self._reps_o.parameters())
+                    + list(self._reps_mu0.parameters())
+                    + list(self._reps_mu1.parameters())
+                    + list(self._reps_prop.parameters())
+                    + list(self._po_estimators[0].parameters())
+                    + list(self._po_estimators[1].parameters())
+                    + list(self._propensity_estimator.parameters())
+            )
+        else:
+            self._reps_o = RepresentationNet(
+                n_unit_in, n_units=n_units_r, n_layers=n_layers_r, nonlin=nonlin,
+                batch_norm=batch_norm
+            )
+
+            for idx in range(2):
+                self._po_estimators.append(
+                    BasicNet(
+                        f"snet_po_estimator_{idx}",
+                        n_units_r
+                        + n_units_r_small,  # (reps_o, reps_mu{idx})
+                        binary_y=binary_y,
+                        n_layers_out=n_layers_out,
+                        n_units_out=n_units_out,
+                        nonlin=nonlin,
+                        batch_norm=batch_norm
+                    )
+                )
+
+            params = (list(self._reps_o.parameters())
+                      + list(self._reps_mu0.parameters())
+                      + list(self._reps_mu1.parameters())
+                      + list(self._po_estimators[0].parameters())
+                      + list(self._po_estimators[1].parameters())
+                      )
+
         self.optimizer = torch.optim.Adam(params, lr=lr, weight_decay=weight_decay)
 
     def loss(
-        self,
-        y0_pred: torch.Tensor,
-        y1_pred: torch.Tensor,
-        t_pred: torch.Tensor,
-        discrepancy: torch.Tensor,
-        y_true: torch.Tensor,
-        t_true: torch.Tensor,
+            self,
+            y0_pred: torch.Tensor,
+            y1_pred: torch.Tensor,
+            t_pred: torch.Tensor,
+            discrepancy: torch.Tensor,
+            y_true: torch.Tensor,
+            t_true: torch.Tensor,
     ) -> torch.Tensor:
         def head_loss(y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
             if self.binary_y:
@@ -204,10 +239,10 @@ class SNet(BaseCATEEstimator):
                 return (y_pred - y_true) ** 2
 
         def po_loss(
-            y0_pred: torch.Tensor,
-            y1_pred: torch.Tensor,
-            y_true: torch.Tensor,
-            t_true: torch.Tensor,
+                y0_pred: torch.Tensor,
+                y1_pred: torch.Tensor,
+                y_true: torch.Tensor,
+                t_true: torch.Tensor,
         ) -> torch.Tensor:
             loss0 = torch.mean((1.0 - t_true) * head_loss(y0_pred, y_true))
             loss1 = torch.mean(t_true * head_loss(y1_pred, y_true))
@@ -215,24 +250,27 @@ class SNet(BaseCATEEstimator):
             return loss0 + loss1
 
         def prop_loss(
-            t_pred: torch.Tensor,
-            t_true: torch.Tensor,
+                t_pred: torch.Tensor,
+                t_true: torch.Tensor,
         ) -> torch.Tensor:
-            t_pred = t_pred + EPS
-            return nn.CrossEntropyLoss()(t_pred, t_true)
+            if self.with_prop:
+                t_pred = t_pred + EPS
+                return nn.CrossEntropyLoss()(t_pred, t_true)
+            else:
+                return 0
 
         return (
-            po_loss(y0_pred, y1_pred, y_true, t_true)
-            + prop_loss(t_pred, t_true)
-            + discrepancy
-            + self._ortho_reg()
+                po_loss(y0_pred, y1_pred, y_true, t_true)
+                + prop_loss(t_pred, t_true)
+                + discrepancy
+                + self._ortho_reg()
         )
 
     def train(
-        self,
-        X: torch.Tensor,
-        y: torch.Tensor,
-        w: torch.Tensor,
+            self,
+            X: torch.Tensor,
+            y: torch.Tensor,
+            w: torch.Tensor,
     ) -> "SNet":
         """
         Fit treatment models.
@@ -272,8 +310,8 @@ class SNet(BaseCATEEstimator):
                 self.optimizer.zero_grad()
 
                 idx_next = train_indices[
-                    (b * batch_size) : min((b + 1) * batch_size, n - 1)
-                ]
+                           (b * batch_size): min((b + 1) * batch_size, n - 1)
+                           ]
 
                 X_next = X[idx_next]
                 y_next = y[idx_next].squeeze()
@@ -303,8 +341,8 @@ class SNet(BaseCATEEstimator):
                         self.loss(
                             y0_preds, y1_preds, prop_preds, discrepancy, y_val, w_val
                         )
-                        .detach()
-                        .cpu()
+                            .detach()
+                            .cpu()
                     )
                     if val_loss_best > val_loss:
                         val_loss_best = val_loss
@@ -325,7 +363,7 @@ class SNet(BaseCATEEstimator):
             return torch.sum(torch.abs(mat), dim=0)
 
         def _get_cos_reg(
-            params_0: torch.Tensor, params_1: torch.Tensor, normalize: bool = False
+                params_0: torch.Tensor, params_1: torch.Tensor, normalize: bool = False
         ) -> torch.Tensor:
             if normalize:
                 params_0 = params_0 / torch.linalg.norm(params_0, dim=0)
@@ -335,56 +373,74 @@ class SNet(BaseCATEEstimator):
             y_min = min(params_0.shape[1], params_1.shape[1])
 
             return (
-                torch.linalg.norm(
-                    params_0[:x_min, :y_min] * params_1[:x_min, :y_min], "fro"
-                )
-                ** 2
+                    torch.linalg.norm(
+                        params_0[:x_min, :y_min] * params_1[:x_min, :y_min], "fro"
+                    )
+                    ** 2
             )
 
-        reps_c_params = self._reps_c.model[0].weight
         reps_o_params = self._reps_o.model[0].weight
         reps_mu0_params = self._reps_mu0.model[0].weight
         reps_mu1_params = self._reps_mu1.model[0].weight
-        reps_prop_params = self._reps_prop.model[0].weight
+
+        if self.with_prop:
+            reps_c_params = self._reps_c.model[0].weight
+            reps_prop_params = self._reps_prop.model[0].weight
 
         # define ortho-reg function
         if self.ortho_reg_type == "abs":
-            col_c = _get_absolute_rowsums(reps_c_params)
             col_o = _get_absolute_rowsums(reps_o_params)
             col_mu0 = _get_absolute_rowsums(reps_mu0_params)
             col_mu1 = _get_absolute_rowsums(reps_mu1_params)
-            col_w = _get_absolute_rowsums(reps_prop_params)
-            return self.penalty_orthogonal * torch.sum(
-                col_c * col_o
-                + col_c * col_w
-                + col_c * col_mu1
-                + col_c * col_mu0
-                + col_w * col_o
-                + col_mu0 * col_o
-                + col_o * col_mu1
-                + col_mu0 * col_mu1
-                + col_mu0 * col_w
-                + col_w * col_mu1
-            )
+            if self.with_prop:
+                col_c = _get_absolute_rowsums(reps_c_params)
+                col_w = _get_absolute_rowsums(reps_prop_params)
+
+                return self.penalty_orthogonal * torch.sum(
+                    col_c * col_o
+                    + col_c * col_w
+                    + col_c * col_mu1
+                    + col_c * col_mu0
+                    + col_w * col_o
+                    + col_mu0 * col_o
+                    + col_o * col_mu1
+                    + col_mu0 * col_mu1
+                    + col_mu0 * col_w
+                    + col_w * col_mu1
+                )
+            else:
+                return self.penalty_orthogonal * torch.sum(
+                    + col_mu0 * col_o
+                    + col_o * col_mu1
+                    + col_mu0 * col_mu1
+                )
 
         elif self.ortho_reg_type == "fro":
-            return self.penalty_orthogonal * (
-                _get_cos_reg(reps_c_params, reps_o_params)
-                + _get_cos_reg(reps_c_params, reps_mu0_params)
-                + _get_cos_reg(reps_c_params, reps_mu1_params)
-                + _get_cos_reg(reps_c_params, reps_prop_params)
-                + _get_cos_reg(reps_o_params, reps_mu0_params)
-                + _get_cos_reg(reps_o_params, reps_mu1_params)
-                + _get_cos_reg(reps_o_params, reps_prop_params)
-                + _get_cos_reg(reps_mu0_params, reps_mu1_params)
-                + _get_cos_reg(reps_mu0_params, reps_prop_params)
-                + _get_cos_reg(reps_mu1_params, reps_prop_params)
-            )
+            if self.with_prop:
+                return self.penalty_orthogonal * (
+                        _get_cos_reg(reps_c_params, reps_o_params)
+                        + _get_cos_reg(reps_c_params, reps_mu0_params)
+                        + _get_cos_reg(reps_c_params, reps_mu1_params)
+                        + _get_cos_reg(reps_c_params, reps_prop_params)
+                        + _get_cos_reg(reps_o_params, reps_mu0_params)
+                        + _get_cos_reg(reps_o_params, reps_mu1_params)
+                        + _get_cos_reg(reps_o_params, reps_prop_params)
+                        + _get_cos_reg(reps_mu0_params, reps_mu1_params)
+                        + _get_cos_reg(reps_mu0_params, reps_prop_params)
+                        + _get_cos_reg(reps_mu1_params, reps_prop_params)
+                )
+            else:
+                return self.penalty_orthogonal * (
+                        + _get_cos_reg(reps_o_params, reps_mu0_params)
+                        + _get_cos_reg(reps_o_params, reps_mu1_params)
+                        + _get_cos_reg(reps_mu0_params, reps_mu1_params)
+                )
+
         else:
             raise ValueError(f"Invalid orth_reg_typ {self.ortho_reg_type}")
 
     def _maximum_mean_discrepancy(
-        self, X: torch.Tensor, w: torch.Tensor
+            self, X: torch.Tensor, w: torch.Tensor
     ) -> torch.Tensor:
         n = w.shape[0]
         n_t = torch.sum(w)
@@ -398,7 +454,7 @@ class SNet(BaseCATEEstimator):
         return torch.sum((mean_treated - mean_control) ** 2)
 
     def _step(
-        self, X: torch.Tensor, w: torch.Tensor
+            self, X: torch.Tensor, w: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         y0_preds, y1_preds, prop_preds, reps_o = self._forward(X)
 
@@ -407,22 +463,27 @@ class SNet(BaseCATEEstimator):
         return y0_preds, y1_preds, prop_preds, discrepancy
 
     def _forward(
-        self, X: torch.Tensor
+            self, X: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        reps_c = self._reps_c(X)
         reps_o = self._reps_o(X)
         reps_mu0 = self._reps_mu0(X)
         reps_mu1 = self._reps_mu1(X)
-        reps_w = self._reps_prop(X)
 
-        reps_po_0 = torch.cat((reps_c, reps_o, reps_mu0), dim=1)
-        reps_po_1 = torch.cat((reps_c, reps_o, reps_mu1), dim=1)
-        reps_w = torch.cat((reps_c, reps_w), dim=1)
+        if self.with_prop:
+            reps_c = self._reps_c(X)
+            reps_w = self._reps_prop(X)
+
+            reps_po_0 = torch.cat((reps_c, reps_o, reps_mu0), dim=1)
+            reps_po_1 = torch.cat((reps_c, reps_o, reps_mu1), dim=1)
+            reps_w = torch.cat((reps_c, reps_w), dim=1)
+            prop_preds = self._propensity_estimator(reps_w)
+        else:
+            reps_po_0 = torch.cat((reps_o, reps_mu0), dim=1)
+            reps_po_1 = torch.cat((reps_o, reps_mu1), dim=1)
+            prop_preds = 0.5 * torch.ones(len(X))  # no probability predictions
 
         y0_preds = self._po_estimators[0](reps_po_0).squeeze()
         y1_preds = self._po_estimators[1](reps_po_1).squeeze()
-
-        prop_preds = self._propensity_estimator(reps_w)
 
         return y0_preds, y1_preds, prop_preds, reps_o
 
