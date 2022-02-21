629b176e3e87deb0c69d74403fd85edc5b073c9b
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-02-21 19:36:51 +0200

    Workflows update: Scheduled runs, updated python versions (#14)
    * bump version
    
    * Bump version: 0.1.6
    
    * Workflows update: Scheduled runs, updated python versions (#2)
    
    * update Py versions
    
    * py3.10 fix
    
    * update deps
    
    * drop 3.10
    
    * updare reqs
    
    * debug jaxlib
    
    * debug jaxlib
    
    * windows debug
    
    * debug windows
    
    * debug windows
    
    * optional JAX, Torch
    
    * debug windows
    
    * debug windows
    
    * bump version

--- catenets/models/__init__.py
+++ catenets/models/__init__.py
@@ -1,3 +1,13 @@
-from . import jax, torch
+import catenets.logger as log
+
+try:
+    from . import jax
+except ImportError:
+    log.error("JAX models disabled")
+
+try:
+    from . import torch
+except ImportError:
+    log.error("PyTorch models disabled")
 
 __all__ = ["jax", "torch"]
